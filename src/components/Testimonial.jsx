5c21ad17d623abb5eb06e00767167631462b755f
faha1999 <kafaha1999@gmail.com>
2022-05-02 21:58:07 +0600

    added swiper js

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import testimonialData from './data/testimonialData';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+
+// css
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const Testimonial = () => {
+  const pagination = {
+    clickable: true,
+    loop: true,
+  };
+
   return (
     <section id="testimonial">
       <h5>Review from my clients</h5>
       <h2>Testimonial</h2>
 
-      <div className="container testimonials">
+      <Swiper
+        className="container testimonials"
+        pagination={pagination}
+        modules={[Pagination]}
+        spaceBetween={40}
+        sliderPerView={1}
+      >
         {testimonialData.map(({ id, image, alt, name, review }) => {
           return (
-            <article className="testimonial" key={id}>
+            <SwiperSlide className="testimonial" key={id}>
               <div className="avatar">
                 <img src={image} alt={alt} />
               </div>
 
               <div className="name">{name}</div>
               <small className="review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
