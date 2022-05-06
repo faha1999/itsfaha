46b81f7c5c8c45ab3e6f816328b18067d0543451
faha1999 <kafaha1999@gmail.com>
2022-05-06 17:08:46 +0600

    added theme propertys

---
+++ src/components/Theme.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BiMoon } from 'react-icons/bi';
+import { BiSun } from 'react-icons/bi';
+
+const Theme = () => {
+  return (
+    <div className="theme">
+      <BiMoon className="icon" />
+      <BiSun className="icon" />
+    </div>
+  );
+};
+
+export default Theme;
